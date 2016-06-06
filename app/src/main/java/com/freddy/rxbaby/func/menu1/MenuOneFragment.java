619ba86e0a9f41956eb8948a8f86cf20841e38d5
freddy <freddywu24@gmail.com>
2016-06-06 18:46:43 +0800

    remove extra jar

--- app/src/main/java/com/freddy/rxbaby/func/menu1/MenuOneFragment.java
+++ app/src/main/java/com/freddy/rxbaby/func/menu1/MenuOneFragment.java
@@ -5,12 +5,10 @@
 package com.freddy.rxbaby.func.menu1;
 
 import android.content.Context;
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
-import android.support.v4.view.PagerAdapter;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
